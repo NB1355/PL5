4ed098d4861e103796c2ee577840b08fe36b514d
NB1355 <nahal5513@gmail.com>
2023-07-28 12:34:40 +0000

    FIX debus, skey update

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'smcjqwguq8f4wonpmd7x55odz=%srjn)^qo2zv+b78y*t6^pp%')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'JxQ0fau0bSl6skOcbMI^XIU62mU#YLmJawU')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG=True
 
-ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost', '8000-nb1355-pl5-pwhpbnlosyj.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost', '8000-nb1355-pl5-9as39bwgjxr.ws-eu102.gitpod.io']
 
 
 # Application definition
